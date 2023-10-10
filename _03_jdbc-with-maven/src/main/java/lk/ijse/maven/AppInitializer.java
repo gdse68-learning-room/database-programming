7b80eccb9e1ef152193a2ae657d1cbcdc97cf8b3
danu <danujagreru@gmail.com>
2023-10-10 14:28:35 +0530

    customer saved

---
+++ _03_jdbc-with-maven/src/main/java/lk/ijse/maven/AppInitializer.java
@@ -0,0 +1,38 @@
+package lk.ijse.maven;
+
+/*
+    @author DanujaV
+    @created 10/10/23 - 2:15 PM   
+*/
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class AppInitializer {
+    private static void saveCustomer(String id, String name, String address, String tel) throws SQLException {
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/kade",
+                "root",
+                "Danu25412541@"
+        );
+
+        String sql = "INSERT INTO customer VALUES(?, ?, ?, ?)";
+        PreparedStatement pstm = connection.prepareStatement(sql);
+        pstm.setString(1, id);
+        pstm.setString(2, name);
+        pstm.setString(3, address);
+        pstm.setString(4, tel);
+
+        int affectedRows = pstm.executeUpdate();
+
+        System.out.println(affectedRows > 0 ? "customer saved!" : "oops! something went wrong!");
+
+        connection.close();
+    }
+
+    public static void main(String[] args) throws SQLException {
+        saveCustomer("C002", "Saman", "Panadura", "0745845627");
+    }
+}
