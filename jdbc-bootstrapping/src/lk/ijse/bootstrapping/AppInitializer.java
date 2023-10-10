36abdd896faf75bd0c4d19a1c9bda53c2383178e
danu <danujagreru@gmail.com>
2023-10-10 10:42:42 +0530

    initial commit

---
+++ jdbc-bootstrapping/src/lk/ijse/bootstrapping/AppInitializer.java
@@ -0,0 +1,12 @@
+package lk.ijse.bootstrapping;
+
+/*
+    @author DanujaV
+    @created 10/10/23 - 10:39 AM   
+*/
+
+public class AppInitializer {
+    public static void main(String[] args) {
+
+    }
+}
